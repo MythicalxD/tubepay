3ff9aac7df15358c0a1c1fd0e282441605a0fc42
Mythicalx <creators462023@gmail.com>
2024-06-24 22:36:55 +0530

    f

--- includes/DbOperations.php
+++ includes/DbOperations.php
@@ -275,7 +275,7 @@ class DbOperations
                ]
           ];
 
-          $stmt = $this->con->prepare("SELECT * FROM videos ORDER BY RAND() WHERE");
+          $stmt = $this->con->prepare("SELECT * FROM videos ORDER BY RAND()");
 
           if ($stmt->execute()) {
                $result = $stmt->get_result();
