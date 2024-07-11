88d0bd274748570de89603741ff9e071b7d5731e
Mythicalx <creators462023@gmail.com>
2024-07-11 22:21:32 +0530

    .

--- v1/pb/cpx.php
+++ v1/pb/cpx.php
@@ -3,7 +3,7 @@
 require_once '../../includes/DbOperations.php';
 
 // Your secret key can be found in your apps section by clicking on the "Secret Key" button
-$secret_key = '63uvZy44aLLiqnYg1V00b8aUkKNwemWy';
+$secret_key = 'GdQ9VX5WkpDotoTj3KuNBqXeEg4uKmoU';
 
 // KiwiWall server IP addresses
 $allowed_ips = array(
@@ -30,6 +30,8 @@ $secure_hash = $_REQUEST['secure_hash'];
 $validation_signature = md5($trans_id . '-' . $secret_key);
 if ($secure_hash != $validation_signature) {
     // Signatures not equal - send error code
+    echo 0;
+    die();
 }
 // Validation was successful. Credit user process.
 $db = new DbOperations();
