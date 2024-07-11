e1cdcd279a1dec90ea056b3887948dce2bb81431
Mythicalx <creators462023@gmail.com>
2024-07-11 21:54:47 +0530

    .

---
+++ v1/pb/cpx.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once '../../includes/DbOperations.php';
+
+// Your secret key can be found in your apps section by clicking on the "Secret Key" button
+$secret_key = '63uvZy44aLLiqnYg1V00b8aUkKNwemWy';
+
+// KiwiWall server IP addresses
+$allowed_ips = array(
+    '188.40.3.73'
+);
+
+if (!in_array($_SERVER['REMOTE_ADDR'], $allowed_ips)) {
+    echo 0;
+    die();
+}
+
+// Get parameters
+$status = $_REQUEST['status'];
+$trans_id = $_REQUEST['trans_id'];
+$user_id = $_REQUEST['user_id'];
+$amount_local = $_REQUEST['amount_local'];
+$amount_usd = $_REQUEST['amount_usd'];
+$ip_click = $_REQUEST['ip_click'];
+$type = $_REQUEST['type'];
+$secure_hash = $_REQUEST['secure_hash'];
+
+
+// Create validation signature
+$validation_signature = md5($trans_id . '-' . $secret_key);
+if ($secure_hash != $validation_signature) {
+    // Signatures not equal - send error code
+}
+// Validation was successful. Credit user process.
+$db = new DbOperations();
+$result = $db->addPointsOfferwall($user_id, $trans_id, $amount_local);
+
+echo 1;
+die();
