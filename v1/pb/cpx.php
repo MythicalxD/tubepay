925b75ab790e80bbac07db351f0a4e19f8c8ea50
Mythicalx <creators462023@gmail.com>
2024-07-11 22:47:56 +0530

    .

--- v1/pb/cpx.php
+++ v1/pb/cpx.php
@@ -1,5 +1,6 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 require_once '../../includes/DbOperations.php';
 
 // Your secret key can be found in your apps section by clicking on the "Secret Key" button
